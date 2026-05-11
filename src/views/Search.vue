2cae786d4a5afec13651c795dd0f0848d894f278
Vsevolod Koltsov <vsevolodkoltsov13@gmail.com>
2026-05-11 16:13:15 +0300

    Исправлен CSS

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -158,6 +158,8 @@ watch(() => route.query.q, (newQ) => {
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
   overflow: hidden;
+  line-clamp: 2;
+  box-orient: vertical;
 }
 
 .empty-state {
